3be6f3691f299ce553b56f1566c2fd39b069d568
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-17 21:24:30 +0200

    fetch bands works

--- src/App.js
+++ src/App.js
@@ -10,14 +10,28 @@ import Whoups404 from "./pages/Whoups404";
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import "./App.css";
+import { useState, useEffect } from "react"
 
 function App() {
+  const [bandsList, setProducts] = useState([]);
+
+  useEffect(()=>{
+    async function getProducts() {
+      const res = await fetch("https://the-javascript-bar-project.herokuapp.com/bands")
+      const data = await res.json();
+      setProducts(data);
+    }
+    getProducts();
+  }, [])
+
+  console.log(bandsList);
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="lineup" element={<Lineup />} />
+        <Route path="lineup" element={<Lineup bands={bandsList}/>} />
         <Route path="artist/:id" element={<Artist />} />
         <Route path="schedule" element={<Schedule />} />
         <Route path="shop" element={<Shop />} />
--- src/pages/Lineup.js
+++ src/pages/Lineup.js
@@ -1,5 +1,13 @@
-export default function Lineup() {
+export default function Lineup({bands}) {
+
    return (
-        <h1 className="text-4xl">Lineup page</h1>
+       <>
+       <h1 className="text-4xl">Lineup</h1>
+        <ul className="w-full grid gap-4 grid-cols-3 grid-rows-3 p-4">
+            {bands.map( band => 
+                <li className="bg-gray-600 text-white rounded-md p-4">{band.name}</li>
+            )}
+        </ul>
+        </>
     )
 }
