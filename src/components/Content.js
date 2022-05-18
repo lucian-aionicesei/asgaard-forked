620d41454bf0cd3f547ba1ce212bd0660d0aaa2d
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-18 11:58:48 +0200

    Added space between coponents in main

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,3 @@
 export default function Content(props) {
-  return <main className="bg-black px-40 text-concert-yellow">{props.children}</main>;
+  return <main className="bg-black px-40 text-concert-yellow space-y-24">{props.children}</main>;
 }
