a2df1fdc579a05cd8375c560d923acc3c72e4ff0
Paul Balta <paul6868@stud.kea.dk>
2022-05-20 15:17:02 +0200

    this is with the text and the other input now making it work

--- src/pages/Lineup.js
+++ src/pages/Lineup.js
@@ -1,32 +1,43 @@
 import Content from "../components/Content";
 import { Button2 } from "../components/Buttons";
-import { Link } from "react-router-dom";
 
 export default function Lineup({ bands }) {
   return (
     <div>
       <div className="py-6 px-6 sm:mx-6 lg:mx-8 ">
-        <h1 className="text-7xl sm:text-7xl font-acier bg-concert-pink flex justify-center py-6 px-6 lg:py-16 px-16 xl:text-[114px] xl:py-10 px-10">Line up</h1>
+        <h1 className="text-7xl sm:text-7xl font-acier bg-concert-yellow flex justify-center py-6 px-6 lg:py-16 px-16 xl:text-[114px] xl:py-10 px-10">Line up</h1>
       </div>
       <Content>
         <section className="m-8 mt-2 xl:m-16">
           <article>
-            <p className="text-center text-base font-semibold lg:text-xl xl:text-2xl">
-              Since it was launched 3 years ago as a student project of four KEA students - Lucian, Christian, Paul and Drago, the festival has changed drastically and become the <b>largest festival in the world</b>. The line up we bring you this
-              year is certainly the biggest gathering of the world's most popular rock stars on one place. In fact, in three places.
+            <p className="text-center text-base font-semibold md:mx-16 lg:text-xl xl:text-2xl 2xl:text-3xl 2xl:mx-56">
+              Since it was launched 3 years ago as a student project of four KEA students - Lucian, Christian, Paul and Drago, the festival has changed drastically and become the <b>largest festival in the world</b>.{" "}
+            </p>
+            <p className="text-center text-base font-semibold md:mx-16 lg:text-xl xl:text-2xl 2xl:text-3xl 2xl:mx-56">
+              The line up we bring you this year is certainly the biggest gathering of the world's most popular rock stars on one place. In fact, in three places.
             </p>
           </article>
-          <p className="flex justify-center text-base font-semibold lg:text-xl">~</p>
+          <p className="flex justify-center text-base font-semibold m-4  md:mx-16 lg:text-xl 2xl:text-4xl 2xl:m-8">~</p>
           <article>
-            <p className="text-center text-base font-semibold lg:text-xl xl:text-2xl">
+            <p className="text-center text-base font-semibold md:mx-16 lg:text-xl xl:text-2xl 2xl:text-3xl 2xl:mx-56">
               Three stages, equally big. The band schedule is such that you can choose one stage and explore diverse artists, otherwise you have enough time to simply switch to other stage The passage between the stages is free.
             </p>
-            <p className="text-center text-base font-semibold pt-6 lg:text-xl xl:text-2xl">On weekends, there will be one big surprise guest on each stage, who is not listed in this year's line-up. See you!</p>
+            <p className="text-center text-base font-semibold pt-6 md:mx-16 lg:text-xl xl:text-2xl 2xl:text-3xl 2xl:mx-56">On weekends, there will be one big surprise guest on each stage, who is not listed in this year's line-up. See you!</p>
           </article>
         </section>
-        <Link to="/artist/2">
-          <Button2 label="See all artists" bgcolor="concert-pink" />
-        </Link>
+
+        <div className="bg-concert-yellow flex flex-row">
+          <div className="flex flex-row p-4">
+            <Button2 label="ALPHABETICAL"></Button2>
+            <Button2 label="GENRE">GENRE</Button2>
+          </div>
+          <div className="p-4">
+            <input type="text" name="SEARCH" placeholder="This is new"></input>
+          </div>
+
+          <div></div>
+        </div>
+
         <ul className="w-full grid gap-4 grid-cols-3 grid-rows-3 p-4">
           {bands.map((band, index) => (
             <li key={`band#${index}`} className="bg-gray-600 text-white rounded-md p-4">
