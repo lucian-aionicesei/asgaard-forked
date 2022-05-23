1b9b7d078fb0a110869b352a43fe78a9ebcec2f8
Drago Tomasic <drag0540@stud.kea.dk>
2022-05-23 14:05:23 +0200

    up

--- src/components/Passes.js
+++ src/components/Passes.js
@@ -3,9 +3,9 @@ export default function Passes() {
     <div className="w-full h-96 bg-concert-pink">
       <h3 className="font-acier p-5 text-6xl text-black">FESTIVAL PASSES</h3>
       <div className="flex box-border flex-col gap-6 pl-12 pr-10">
-        <div className="flex relative h-14 w-full bg-white border-[3px] border-black">
-          <div className="flex absolute top-0 left-0 right-0 bottom-0 h-auto w-10 bg-concert-l-green border-r-[2px] border-black"></div>
-          <div className="flex absolute left-0 right-0 top-0 bottom-0 ml-10 items-center m-auto">
+        <div className="flex relative h-auto phone:h-14 w-full bg-white border-[3px] border-black">
+          <div className="flex h-auto w-10 bg-concert-l-green border-r-[2px] border-black"></div>
+          <div className="flex ml-10 items-center">
             <div className="flex flex-col sm:flex-row">
               <div className="text-xs pl-4 md:text-sm pl-4 text-black font-bold">
                 GENERAL ACCES
@@ -14,7 +14,7 @@ export default function Passes() {
                 140$
               </div>
             </div>
-            <div className="ml-0 sm:flex flex-col justify-center align-center sm:flex-row">
+            <div className=" flex flex-col ml-auto justify-end align-center sm:flex-row">
               <div className="flex justify-center ml-auto mr-5 justify-around items-center text-center w-20 h-3/5 bg-concert-yellow border-black border-[2px]">
                 <div className="w-4 h-4 bg-concert-yellow border-black border-[1px] rounded-full"></div>
                 <div className="align-middle text-black">1</div>
