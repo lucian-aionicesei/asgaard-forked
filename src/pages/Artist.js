57b4fcd61f3b5548c1c8ad2d587af5859803f53d
Paul Balta <paul6868@stud.kea.dk>
2022-05-24 21:42:14 +0200

    now it shows the id

--- src/pages/Artist.js
+++ src/pages/Artist.js
@@ -2,12 +2,12 @@ import Content from "../components/Content";
 import { useParams } from "react-router-dom";
 
 export default function Artist({ bands }) {
-  const params = useParams();
-  console.log(params);
+  let { id } = useParams();
+  console.log(id);
 
   return (
     <Content>
-      <h1 className="text-7xl font-acier">this is {params.userId}</h1>
+      <h1 className="text-7xl font-acier">this is {id.trim()}</h1>
     </Content>
   );
 }
