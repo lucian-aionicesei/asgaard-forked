2f8b582c420b3198dde40814b5561cd83db7662b
Paul Balta <paul6868@stud.kea.dk>
2022-05-25 19:59:10 +0200

    now the artist page is working very good

--- src/pages/Artist.js
+++ src/pages/Artist.js
@@ -38,17 +38,20 @@ export default function Artist({ bands }) {
       {/* <CheckingBandPage bands={bands} /> */}
       <div className="flex justify-center items-center pt-[4rem] ">
         <div className="w-[95%] sm:w-[90%]  lg:w-3/4 xl:w-[70%]  shadow-[0_4px_80px_35px_rgba(251,206,48,0.25)]">
-          <h1 className="font-aciersolid  text-2xl lg:font-acier text-center py-4 hover:font-aciersolid  xl:text-8xl border-[2px] border-b-[0px] border-concert-yellow bg-concert-pink text-black "> {MakeIDCapialLetter}</h1>
+          <h1 className="font-aciersolid  text-2xl lg:font-acier text-center py-4 hover:font-aciersolid md:text-3xl lg:text-5xl xl:text-6xl 2xl:text-8xl border-[2px] border-b-[0px] border-concert-yellow bg-concert-pink text-black ">
+            {" "}
+            {MakeIDCapialLetter}
+          </h1>
           <div className=" p-2  flex flex-row justify-between border-[2px] border-concert-yellow lg:p-4 xl:p-4 2xl:p-6">
             <div className="flex justify-center items-center">
               <div className="px-3 sm:px-4 flex justify-center items-center text-black font-aciersolid lg:px-14 py-1 w-fit font-bold border-[3px] border-black text-lg bg-concert-yellow mr-2 2xl:text-4xl tracking-widest">{thisBand.genre}</div>
             </div>
-            <div className=" flex flex-row  items-center m-2 w-[38%]  ">
-              <BsInstagram size={32} className="mr-8 text-concert-pink size={40}" />
-              <BsSpotify size={32} className="mr-8 text-concert-pink" />
-              <ImSoundcloud size={48} className="mr-8 text-concert-pink" />
+            <div className=" flex flex-row  items-center m-2 w-[38%]   ">
+              <BsInstagram size={32} className="mr-8 2xl:mr-2 text-concert-pink size={40}" />
+              <BsSpotify size={32} className="mr-8 2xl:mr-2  text-concert-pink" />
+              <ImSoundcloud size={48} className="mr-8 2xl:mr-2 text-concert-pink" />
             </div>
-            <div className=" text-xl flex items-center font-acier text-concert-pink xl:text-5xl p-2">STAGE 1</div>
+            <div className=" text-xl flex items-center font-acier text-concert-pink md:text-2xl xl:text-5xl p-2">STAGE 1</div>
           </div>
           <div>
             <div className="h-[17rem] lg:h-[30rem] border-[2px] border-b-[0px] border-t-[0px] border-concert-yellow ">
@@ -56,11 +59,11 @@ export default function Artist({ bands }) {
             </div>
           </div>
           <div className="border-[2px] border-b-[0px] border-concert-yellow flex items-center justify-center p-8">
-            <div className="w-[70%] bg-concert-pink text-center h-20 flex items-center justify-center xl:text-4xl font-aciersolid text-black">SPOTIFY</div>
+            <div className="w-[90%] md:w-[80%] lg:w-[70%] bg-concert-pink text-center h-20 flex items-center justify-center xl:text-4xl font-aciersolid text-black">SPOTIFY</div>
           </div>
           <div className="">
             <section>
-              <p className="border-[2px] border-concert-yellow p-8 text-montserrat xl:text-2xl xl:font-semibold xl:leading-loose">{thisBand.bio}</p>
+              <p className="border-[2px] border-concert-yellow p-8 text-montserrat md:text-lg lg:text-xl lg:font-semibold xl:text-2xl xl:font-semibold xl:leading-loose">{thisBand.bio}</p>
             </section>
           </div>
         </div>
