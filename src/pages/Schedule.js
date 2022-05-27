a4c73d723f96e88c658becbac0d666024755ab35
Paul Balta <paul6868@stud.kea.dk>
2022-05-27 13:56:38 +0200

    this is schedule

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -8,7 +8,7 @@ export default function Schedule() {
       </div>
       <Content>
         <div>
-          <div className="flex flex-row justify-center ">
+          <div className="flex flex-row justify-center">
             <div className=" bg-concert-redish px-[0.3rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black mb-3 mt-2 mr-1">MONDAY</div>
             <div className=" bg-concert-redish  px-[0.3rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black mb-3 mt-2 mr-1">TUESDAY</div>
             <div className=" bg-concert-redish  px-[0.3rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black mb-3 mt-2 mr-1">WEDNSDAY</div>
@@ -22,15 +22,15 @@ export default function Schedule() {
           </div>
           <div className="text-5xl sm:text-7xl font-acier bg-concert-redish flex justify-start py-1 px-1 lg:py-1 px-1 xl:text-[114px] xl:py-1 px-1 text-black">Stage 1</div>
           <div className="bg-concert-redish h-[10rem]">
-            <div className="flex flex-row ">
-              <div className=" bg-concert-yellow px-[0.3rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black mb-3 mt-2 mr-1">12 A.M - 02 P.M.</div>
+            <div className="flex flex-row bg-concert-yellowish p-0 ">
+              <div className=" bg-concert-yellow px-[0.3rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black flex justify-center items-center mr-1">12 A.M - 02 P.M.</div>
               <div className="flex flex-row">
-                <div className="font-montserrat font-extrabold text-black">SAD NIGHT DYNAMITE</div>
+                <div className=" flex justify-center items-center font-montserrat font-extrabold text-black">SAD NIGHT DYNAMITE</div>
                 <div>
-                  <div className=" bg-concert-yellow  px-[0.3rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black mb-3 mt-2 mr-1">POP</div>
+                  <div className=" bg-concert-yellow  px-[0.6rem] py-[0.2rem] text-[9px] font-montserrat font-bold 2xl:px-[4.2rem] text-black mb-3 mt-2 mr-1 ml-2">POP</div>
                 </div>
                 <div>
-                  <img></img>
+                  <img src="/public/images/logos/720px-Queen_–_montagem_–_new.png" alt="img"></img>
                 </div>
               </div>
             </div>
