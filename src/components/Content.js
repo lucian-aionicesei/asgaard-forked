f25abb8cafe99ed24b1e103811484d8bcd7debc0
Drago Tomasic <drag0540@stud.kea.dk>
2022-05-27 14:25:29 +0200

    space y in main changed

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,7 +5,7 @@ import { FLOWBASEANNOTATION_TYPES } from "@babel/types";
 
 export default function Content(props) {
   return (
-    <main className="bg-black px-4 phone:px-8 xl:w-[80%] lg:w-[85%] mx-auto text-concert-yellow space-y-16">
+    <main className="bg-black px-8 phone:px-8 xl:w-[80%] lg:w-[85%] mx-auto text-concert-yellow space-y-6">
       {props.children}
     </main>
   );
