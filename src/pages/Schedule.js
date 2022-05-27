0bd8d837e8658aff3fc8c60889b5b5083a0c6699
Paul Balta <paul6868@stud.kea.dk>
2022-05-27 12:48:49 +0200

    now is working even more

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -7,7 +7,11 @@ export default function Schedule() {
         <h1 className="text-5xl sm:text-7xl font-acier bg-concert-yellow flex justify-center py-6 px-6 lg:py-16 px-16 xl:text-[114px] xl:py-10 px-10">Daily Schedule</h1>
       </div>
       <Content>
-        <div></div>
+        <div>
+          <div className="py-6 px-6 sm:mx-6 lg:mx-8 ">
+            <h1 className="text-5xl sm:text-7xl font-acier bg-concert-redish flex justify-center py-6 px-6 lg:py-16 px-16 xl:text-[114px] xl:py-10 px-10 text-black">MONDAY</h1>
+          </div>
+        </div>
       </Content>
     </div>
   );
