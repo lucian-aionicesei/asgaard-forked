8e76b4baec017691f4fae270850fb264405c762e
Paul Balta <paul6868@stud.kea.dk>
2022-05-27 12:46:14 +0200

    this is working for the schedule

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,9 +1,14 @@
 import Content from "../components/Content";
 
 export default function Schedule() {
-    return (
-        <Content>
-        <h1 className="text-7xl font-acier">Schedule page</h1>
-        </Content>
-    )
+  return (
+    <div>
+      <div className="py-6 px-6 sm:mx-6 lg:mx-8 ">
+        <h1 className="text-5xl sm:text-7xl font-acier bg-concert-yellow flex justify-center py-6 px-6 lg:py-16 px-16 xl:text-[114px] xl:py-10 px-10">Daily Schedule</h1>
+      </div>
+      <Content>
+        <div></div>
+      </Content>
+    </div>
+  );
 }
