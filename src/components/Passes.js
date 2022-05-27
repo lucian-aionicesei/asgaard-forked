7405281fabb104e3900e5318164cd62e34fdd9d7
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-27 19:06:21 +0200

    adjustments to tickets

--- src/components/Passes.js
+++ src/components/Passes.js
@@ -1,55 +1,70 @@
+import { Button1, Button2 } from "./Buttons";
 import PassesLines from "./PassesLines";
+import { useState } from "react";
 
 export default function Passes() {
-  return (
-    <article className="w-full h-auto bg-concert-pink text-black ">
-      <h1 className="font-acier p-5 text-6xl text-black">FESTIVAL PASSES</h1>
-      <PassesLines
-        passName="GENERAL ACCESS"
-        passPrice="140$"
-        bgPasses="concert-l-green"
-        dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat."
-      />
+  const [toggleHidden, setToggleHidden] = useState(false);
 
-      <PassesLines
-        passName="VIP"
-        passPrice="140$"
-        bgPasses="concert-yellowish"
-        dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  return (
+    <article className="w-full h-auto bg-concert-pink text-black pb-[3vw]">
+      <h1 className="font-acier p-5 text-6xl text-black">FESTIVAL PASSES</h1>
+      <ul className="space-y-6 px-[5vw]">
+        <PassesLines
+          passName="GENERAL ACCESS"
+          passPrice="140$"
+          bgPasses="concert-l-green"
+          dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         aliquip ex ea commodo consequat."
-      />
-      <PassesLines
-        passName="PREMIUM PASS"
-        passPrice="140$"
-        bgPasses="concert-redish"
-        dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        />
+
+        <PassesLines
+          passName="VIP"
+          passPrice="140$"
+          bgPasses="concert-yellowish"
+          dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         aliquip ex ea commodo consequat."
-      />
-      <PassesLines
-        passName="DAY PASS"
-        passPrice="50$"
-        bgPasses="concert-red-dark"
-        dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        />
+        <PassesLines
+          passName="PREMIUM PASS"
+          passPrice="140$"
+          bgPasses="concert-redish"
+          dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         aliquip ex ea commodo consequat."
-      />
-      <PassesLines
-        passName="3 DAY PASS"
-        passPrice="120$"
-        bgPasses="concert-b-green"
-        dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        />
+        {toggleHidden && (
+          <>
+            <PassesLines
+              passName="DAY PASS"
+              passPrice="50$"
+              bgPasses="concert-red-dark"
+              dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         aliquip ex ea commodo consequat."
-      />
+            />
+            <PassesLines
+              passName="3 DAY PASS"
+              passPrice="120$"
+              bgPasses="concert-b-green"
+              dropText=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat."
+            />
+          </>
+        )}
+      </ul>
+      <button className="p-5" onClick={() => {
+                setToggleHidden(!toggleHidden);
+              }}>
+        <Button2 label={toggleHidden ? "Hide" : "See all options"} bgcolor="concert-yellow" />
+      </button>
     </article>
   );
 }
--- src/components/PassesLines.js
+++ src/components/PassesLines.js
@@ -10,34 +10,33 @@ export default function PassesLines({
 }) {
   const [show, setShow] = useState(false);
   return (
-    <section className="w-full p-2 font-bold">
-      <div className="w-full h-14 bg-concert-bg border-[3px] border-black flex">
+    <li className="w-full font-bold">
+      <div className="w-full h-16 bg-concert-bg border-[3px] border-black flex">
         <div
           className={`h-full w-10 bg-${bgPasses} border-r-[2px] border-black`}
         ></div>
-        <div className="px-2 justify-between w-full flex h-full">
-          <div className="pt-2 flex text-xs items-center pb-1 space-x-[5vw] sm:text-sm">
-            <div className="w-24">
-              <h4 className="whitespace-pre">{`${passName}`}</h4>
-
+        <div className="px-3 justify-between w-full flex h-full">
+          <div className=" flex text-xs items-center pb-1 space-x-[5vw] sm:text-sm">
+            <div className="w-40">
+              <h4 className="whitespace-pre text-lg">{`${passName}`}</h4>
               <button
                 onClick={() => {
                   setShow(!show);
                 }}
-                className="mt-1 mb-1"
+                /* className="mt-1 mb-1" */
               >
                 <SeeDetailButton label="See Details" />
               </button>
             </div>
-            <p className="pr-2">{`${passPrice}`}</p>
+            <p className="pr-2 text-base">{`${passPrice}`}</p>
           </div>
           <div className="flex h-full items-center space-x-2">
-            <div className="flex items-center h-fit space-x-1 px-1 bg-concert-yellow border-black border-[3px] phone:space-x-2">
-              <AiOutlineMinusCircle />
+            <div className="flex items-center h-8 space-x-2 px-1 bg-concert-yellow border-black border-[2px] phone:space-x-5">
+              <AiOutlineMinusCircle className="text-xl"/>
               <p>1</p>
-              <AiOutlinePlusCircle />
+              <AiOutlinePlusCircle className="text-xl"/>
             </div>
-            <div className="text-[0.3rem] bg-black h-fit text-bold p-1 text-center text-white sm:text-xs">
+            <div className="text-[0.3rem] bg-black h-8 text-bold p-1 flex items-center text-white sm:text-xs">
               ADD TO CHART
             </div>
           </div>
@@ -55,6 +54,6 @@ export default function PassesLines({
           </p>
         </div>
       )}
-    </section>
+    </li>
   );
 }
