fa8923a1973641b4a39d13b53843a3466e7ec918
Drago Tomasic <drag0540@stud.kea.dk>
2022-05-31 13:22:10 +0200

    camping booking animation fin

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -10,48 +10,89 @@ import useFetch from "../hooks/useFetch";
 import Passes from "../components/Passes";
 import useCountdown from "../hooks/useCountdown";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 // const targetDate = this;
 const targetDate = new Date("2022, 07, 20");
 
-export default function Shop({userCart, setUserCart, countdown, setCountdown}) {
+export default function Shop({
+  userCart,
+  setUserCart,
+  countdown,
+  setCountdown,
+}) {
   const [days, hours, minutes, seconds] = useCountdown(targetDate);
-  const { loading, error, data } = useFetch("https://the-javascript-bar-project.herokuapp.com/available-spots");
+  const { loading, error, data } = useFetch(
+    "https://the-javascript-bar-project.herokuapp.com/available-spots"
+  );
 
   return (
     <Content>
       <div className="font-acier text-center pt-20 pb-10">
-      <p className="text-4xl font-aciersolid">
-        {days} Days {hours}h {minutes}' {seconds}"
-      </p>
-      <h1 className="text-6xl md:text-7xl">
-        until impact
-      </h1>
-      <p className="text-2xl ">HUSTON WE GOT THE TICKETS</p>
+        <p className="text-4xl font-aciersolid">
+          {days} Days {hours}h {minutes}' {seconds}"
+        </p>
+        <h1 className="text-6xl md:text-7xl">until impact</h1>
+        <p className="text-2xl ">HUSTON WE GOT THE TICKETS</p>
       </div>
-      <Passes userCart={userCart} setUserCart={setUserCart} countdown={countdown} setCountdown={setCountdown}/>
-      <CampContainer userCart={userCart} setUserCart={setUserCart}/>
+      <Passes
+        userCart={userCart}
+        setUserCart={setUserCart}
+        countdown={countdown}
+        setCountdown={setCountdown}
+      />
+      <CampContainer userCart={userCart} setUserCart={setUserCart} />
       <MerchOffer />
     </Content>
   );
 }
 
-export function CampContainer({userCart, setUserCart}) {
+export function CampContainer({ userCart, setUserCart }) {
   return (
     <div className={`w-full bg-concert-yellow text-black pb-[3vw]`}>
       <h1 className="text-5xl font-acier p-5">CAMPING ACCOMODATION</h1>
       <ul className="space-y-6 px-[5vw] max-w-[1200px]">
-        <CampingLines userCart={userCart} setUserCart={setUserCart} accomodationArea="Svartheim" dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-        <CampingLines userCart={userCart} setUserCart={setUserCart} accomodationArea="Nilfheim" dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-        <CampingLines userCart={userCart} setUserCart={setUserCart} accomodationArea="Helheim" dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-        <CampingLines userCart={userCart} setUserCart={setUserCart} accomodationArea="Muspelheim" dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-        <CampingLines userCart={userCart} setUserCart={setUserCart} accomodationArea="Alfheim" dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+        <CampingLines
+          userCart={userCart}
+          setUserCart={setUserCart}
+          accomodationArea="Svartheim"
+          dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+        <CampingLines
+          userCart={userCart}
+          setUserCart={setUserCart}
+          accomodationArea="Nilfheim"
+          dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+        <CampingLines
+          userCart={userCart}
+          setUserCart={setUserCart}
+          accomodationArea="Helheim"
+          dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+        <CampingLines
+          userCart={userCart}
+          setUserCart={setUserCart}
+          accomodationArea="Muspelheim"
+          dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+        <CampingLines
+          userCart={userCart}
+          setUserCart={setUserCart}
+          accomodationArea="Alfheim"
+          dropText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
       </ul>
     </div>
   );
 }
 
-export function CampingLines({userCart, setUserCart, dropText, accomodationArea}) {
+export function CampingLines({
+  userCart,
+  setUserCart,
+  dropText,
+  accomodationArea,
+}) {
   const [dropDown, setDropDown] = useState(false);
   const [show, setShow] = useState(false);
 
@@ -76,8 +117,26 @@ export function CampingLines({userCart, setUserCart, dropText, accomodationArea}
             </p>
           </div>
           <div className="flex h-fit w-full justify-between items-end">
-            <button onClick={()=>{setShow(!show); setDropDown(false)}}>{show ? <SeeDetailButtonUp label="See details"/> : <SeeDetailButton label="See details"/>}</button>
-            <button onClick={()=>{setDropDown(!dropDown); setShow(false)}}><Button2 label="Book" bgcolor="concert-pink" /></button>
+            <button
+              onClick={() => {
+                setShow(!show);
+                setDropDown(false);
+              }}
+            >
+              {show ? (
+                <SeeDetailButtonUp label="See details" />
+              ) : (
+                <SeeDetailButton label="See details" />
+              )}
+            </button>
+            <button
+              onClick={() => {
+                setDropDown(!dropDown);
+                setShow(false);
+              }}
+            >
+              <Button2 label="Book" bgcolor="concert-pink" />
+            </button>
           </div>
         </div>
       </div>
@@ -93,18 +152,26 @@ export function CampingLines({userCart, setUserCart, dropText, accomodationArea}
           </p>
         </div>
       )}
-      {dropDown && <div className="w-full items-center flex flex-col md:flex-row bg-yellow-50 px-[5vw] py-[4vw] gap-x-[5vw] gap-y-[7vw]">
-        <CampingOption userCart={userCart} setUserCart={setUserCart}
-          accomodationPrice={299}
-          accomodationArea={accomodationArea}
-          accomodationType="2 persons tent"
-          cartItemId={`2pers-${accomodationArea}`}/>
-        <CampingOption userCart={userCart} setUserCart={setUserCart}
-          accomodationPrice={399}
-          accomodationArea={accomodationArea}
-          accomodationType="3 persons tent"
-          cartItemId={`3pers-${accomodationArea}`}/>
-      </div>}
+      {dropDown && (
+        <div className="w-full items-center flex flex-col md:flex-row bg-yellow-50 px-[5vw] py-[4vw] gap-x-[5vw] gap-y-[7vw]">
+          <CampingOption
+            userCart={userCart}
+            setUserCart={setUserCart}
+            accomodationPrice={299}
+            accomodationArea={accomodationArea}
+            accomodationType="2 persons tent"
+            cartItemId={`2pers-${accomodationArea}`}
+          />
+          <CampingOption
+            userCart={userCart}
+            setUserCart={setUserCart}
+            accomodationPrice={399}
+            accomodationArea={accomodationArea}
+            accomodationType="3 persons tent"
+            cartItemId={`3pers-${accomodationArea}`}
+          />
+        </div>
+      )}
     </li>
   );
 }
@@ -119,7 +186,15 @@ const oneCartItem = {
   quantity: 0,
 };
 
-function CampingOption({userCart, setUserCart, cartItemId, accomodationPrice, accomodationArea, accomodationType}) {
+function CampingOption({
+  userCart,
+  setUserCart,
+  cartItemId,
+  accomodationPrice,
+  accomodationArea,
+  accomodationType,
+}) {
+  const [itemAdded, setItemAdded] = useState(false);
   let [itemQuantity, setItemQuantity] = useState(1);
 
   function handleSubmit(event) {
@@ -137,7 +212,12 @@ function CampingOption({userCart, setUserCart, cartItemId, accomodationPrice, ac
     // console.log(countdown);
 
     addToCart(accomodation);
-    // !countdown && setCountdown(true);
+    setItemAdded(true);
+    const interval = setInterval(() => {
+      setItemAdded(false);
+    }, 5000);
+    interval();
+    clearInterval(interval);
     setItemQuantity(1);
   }
 
@@ -161,44 +241,79 @@ function CampingOption({userCart, setUserCart, cartItemId, accomodationPrice, ac
   }
 
   return (
-    <div className=" md:w-full font-semibold bg-yellow-200 pb-2">
-          <img src="./images/camping.jpg" alt="" className="w-full h-40 md:h-[16vw] object-cover border-black border-[2px]"/>
-          <p className="text-base pt-2 px-2">{accomodationType}</p>
-          <p className="text-base px-2">
-              <span className="text-xl font-bold">{accomodationPrice}</span> DKK
-            </p>
-          <form
-            className="flex items-center justify-center space-x-2 pt-4"
-            onSubmit={handleSubmit}
+    <div className=" md:w-full font-semibold bg-yellow-200">
+      <img
+        src="./images/camping.jpg"
+        alt=""
+        className="w-full h-40 md:h-[16vw] object-cover border-black border-[2px]"
+      />
+      <div className="relative overflow-hidden pb-2">
+        <p className="text-base pt-2 px-2">{accomodationType}</p>
+        <p className="text-base px-2">
+          <span className="text-xl font-bold">{accomodationPrice}</span> DKK
+        </p>
+        <form
+          className="flex items-center justify-center space-x-2 pt-4"
+          onSubmit={handleSubmit}
+        >
+          <label className="flex items-center h-8 px-1 bg-concert-yellow border-black border-[2px] selectQuantity">
+            <AiOutlineMinusCircle
+              className="text-xl cursor-pointer"
+              onClick={() =>
+                itemQuantity > 1 && setItemQuantity(itemQuantity - 1)
+              }
+            />
+            <input
+              type="number"
+              min="1"
+              max="100"
+              required
+              value={itemQuantity}
+              onChange={(e) => setItemQuantity(e.target.value)}
+              onBlur={(e) => e.target.value === "" && setItemQuantity(1)}
+              className="bg-concert-yellow text-center p-0 font-bold w-10 border-none"
+            />
+            <AiOutlinePlusCircle
+              className="text-xl cursor-pointer"
+              onClick={() =>
+                itemQuantity >= 1 && setItemQuantity(itemQuantity + 1)
+              }
+            />
+          </label>
+          <button className=" bg-black h-8 font-bold p-1 flex items-center text-white text-xs">
+            ADD TO CHART
+          </button>
+        </form>
+        {itemAdded && (
+          <motion.div
+            animate={{ x: [0, 1000, 0] }}
+            initial={{ left: -1000 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 6,
+              times: [0, 0.2, 1],
+            }}
+            className={`absolute left-0 top-0 w-full h-full flex items-center justify-center bg-concert-redish`}
           >
-            <label className="flex items-center h-8 px-1 bg-concert-yellow border-black border-[2px] selectQuantity">
-              <AiOutlineMinusCircle
-                className="text-xl cursor-pointer"
-                onClick={() =>
-                  itemQuantity > 1 && setItemQuantity(itemQuantity - 1)
-                }
-              />
-              <input
-                type="number"
-                min="1"
-                max="100"
-                required
-                value={itemQuantity}
-                onChange={(e) => setItemQuantity(e.target.value)}
-                onBlur={(e) => e.target.value === "" && setItemQuantity(1)}
-                className="bg-concert-yellow text-center p-0 font-bold w-10 border-none"
-              />
-              <AiOutlinePlusCircle
-                className="text-xl cursor-pointer"
-                onClick={() =>
-                  itemQuantity >= 1 && setItemQuantity(itemQuantity + 1)
-                }
-              />
-            </label>
-            <button className=" bg-black h-8 font-bold p-1 flex items-center text-white text-xs">
-              ADD TO CHART
-            </button>
-          </form>
-        </div>
-  )
+            <p className="text-xl">Item added to cart</p>
+          </motion.div>
+        )}
+      </div>
+    </div>
+  );
 }
+
+/*handleSubmit && (
+  <motion.div
+    animate={{ x: [0, 1000, 0] }}
+    initial={{ left: -1000 }}
+    transition={{
+      ease: "easeInOut",
+      duration: 6,
+      times: [0, 0.2, 1],
+    }}
+    className="absolute left-0 top-0 w-full h-full flex items-center justify-center bg-black"
+  >
+    <p className="text-xl">Item added to cart</p>
+  </motion.div>
+)*/
