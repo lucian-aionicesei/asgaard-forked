d273633184231df3120a0fcaaf7c90fda7713cb9
Paul Balta <paul6868@stud.kea.dk>
2022-06-02 19:31:30 +0200

    this is okay now

--- src/components/Merchandise.js
+++ src/components/Merchandise.js
@@ -6,22 +6,13 @@ export default function Merchandise() {
   return (
     <article className="flex items-center justify-between w-full h-auto bg-concert-redish text-black 2xl:px-36">
       <div className=" w-2/4 p-4 sm:p-8 md:p-12">
-        <img
-          className="border-[2px] border-black object-scale-down w-96"
-          src={Merch}
-          alt=""
-        />
+        <img className="border-[2px] border-black object-scale-down w-96" src={Merch} alt="" />
       </div>
       <div className="w-2/4 p-2 md:p-14  ">
-        <h1 className="text-xl flex flex-col font-acier text-black whitespace-pre sm:text-4xl lg:text-5xl ">
-          Merchandise
-        </h1>
-        <p className="text-sm sm:text-base">
-          Festival products can be purchased at the official webshop as well as
-          at the festival.
-        </p>
+        <h1 className="text-xl flex flex-col font-acier text-black whitespace-pre sm:text-4xl lg:text-5xl ">Merchandise</h1>
+        <p className="text-sm sm:text-base">Festival products can be purchased at the official webshop as well as at the festival.</p>
         <div className="text-sm pt-4">
-          <Link to="/schedule">
+          <Link to="/shop">
             <Button3 label="GET MERCH" buttonThreeBg="concert-yellow" />
           </Link>
         </div>
